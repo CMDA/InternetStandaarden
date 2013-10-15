459e1aa703b41e990643e13f576d29f062760eb7
Justus Sturkenboom <justus@ju5tu5.nl>
2013-10-15 09:08:32 +0200

    hc6

---
+++ hc_week_6/voorbeelden/test.js
@@ -0,0 +1,5 @@
+jQuery(function($){
+	
+	alert('Hallo wereld!');
+
+});
---
+++ hc_week_6/voorbeelden/vb1/vb1.js
@@ -0,0 +1,13 @@
+/**
+* Hoorcollege 7: Voorbeeld 1
+* 	console.log om jQuery te testen
+* 
+* J.P. Sturkenboom <j.p.sturkenboom@hva.nl>
+* Copyleft 2011, all wrongs reversed
+*/
+
+jQuery(function($) {
+	
+	console.log('Hallo wereld!');
+	
+});
