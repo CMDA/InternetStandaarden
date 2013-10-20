f45ed0e6fbaa903640740d2bde9435692e3df1f0
Justus Sturkenboom <justus@ju5tu5.nl>
2013-10-20 09:02:47 +0200

    laatste college

--- hc_week_6/voorbeelden/vb2/vb2.js
+++ hc_week_6/voorbeelden/vb2/vb2.js
@@ -3,23 +3,23 @@
 * 	Manipulatie van een CSS eigenschap en
 *	dynamisch toekennen en verwijderen van
 *	een class.
-* 
+*
 * J.P. Sturkenboom <j.p.sturkenboom@hva.nl>
 * Copyleft 2011, all wrongs reversed
 */
 
 jQuery(function($) {
-	
+
 	$('h1, h2').click(function(event) {
-		
+
 		$(this).css('color', '#ff0000');
-		
+
 	});
-	
+
 	$('p').click(function(event) {
-		
+
 		$(this).toggleClass('cursief');
-		
+
 	});
-	
+
 });
